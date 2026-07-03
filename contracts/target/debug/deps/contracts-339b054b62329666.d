0d406c7e460b0401b1d11ce99b1d0acb59f9724b
tali-creator <talinanzing111@gmail.com>
2026-07-03 14:42:15 +0100

    Fix gitignore, drop build artifacts from tracking

--- contracts/target/debug/deps/contracts-339b054b62329666.d
+++
@@ -1,5 +0,0 @@
-/home/lawless-pad/Documents/my drips projects/stellar-cascade/contracts/target/debug/deps/contracts-339b054b62329666.d: src/lib.rs
-
-/home/lawless-pad/Documents/my drips projects/stellar-cascade/contracts/target/debug/deps/libcontracts-339b054b62329666.rmeta: src/lib.rs
-
-src/lib.rs:
--- contracts/target/debug/deps/contracts-81f5b68ee54f4f72.d
+++
@@ -1,5 +0,0 @@
-/home/lawless-pad/Documents/my drips projects/stellar-cascade/contracts/target/debug/deps/contracts-81f5b68ee54f4f72.d: src/lib.rs
-
-/home/lawless-pad/Documents/my drips projects/stellar-cascade/contracts/target/debug/deps/libcontracts-81f5b68ee54f4f72.rmeta: src/lib.rs
-
-src/lib.rs:
